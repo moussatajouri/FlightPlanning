adbf4bfba55ba0c23dd6482d759cea84b4cf7f5d
PC-Moussa\Moussa <moussa.taj@gmail.com>
2019-01-17 20:20:00 +0100

    Init Solution and Flights service

---
+++ FlightPlanning/FlightPlanning.Services.Flights.Tests/Integration/Controllers/HomeControllerTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FlightPlanning.Services.Flights.Tests.Integration.Controllers
+{
+    public class HomeControllerTests
+    {
+        private readonly TestServer _server;
+        private readonly HttpClient _client;
+
+        public HomeControllerTests()
+        {
+            _server = new TestServer(WebHost.CreateDefaultBuilder()
+                .UseStartup<Startup>());
+            _client = _server.CreateClient();
+        }
+
+        [Fact]
+        public async Task Should_Get_Return_String_Content_When_Root_Path()
+        {
+            var response = await _client.GetAsync("/");
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal("FlightPlanning Flights Service", content);
+        }
+    }
+}
